90e7fbf96733b3519c821777641b0986de44130d
Ajelmar Medina <ajelmarmedina@gmail.com>
2024-03-19 15:12:38 +0800

    Chapter 5

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page() {
+  return (
+    <main className="flex min-h-full max-h-full min-w-full items-center justify-center">
+      <h1 className="text-[6rem] font-medium">Customers Page</h1>
+    </main>
+  )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page() {
+  return (
+    <main className="flex min-h-full max-h-full min-w-full items-center justify-center">
+      <h1 className="text-[6rem] font-medium">Invoices Page</h1>
+    </main>
+  )
+}
