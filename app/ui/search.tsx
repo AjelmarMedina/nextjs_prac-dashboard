7a60e251eee1a601ac9dd0722c5edb1bce92ceeb
Ajelmar Medina <ajelmarmedina@gmail.com>
2024-03-27 11:50:15 +0800

    Search debouncing

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,18 +2,19 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
     if (term) params.set('query', term)
     else params.delete('query');
-    replace(`${pathname}?${params.toString()}`)
-  }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
