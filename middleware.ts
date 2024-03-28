287d0357c7f12f3b17d0fe74de99ddb0cae258b6
Ajelmar Medina <ajelmarmedina@gmail.com>
2024-03-28 22:40:26 +0800

    Added auth

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+
+export default NextAuth(authConfig).auth;
+
+export const config = {
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  /*
+  * Match all request paths except for the ones starting with:
+  * - api (API routes)
+  * - _next/static (static files)
+  * - _next/image (image optimization files)
+  * - favicon.ico (favicon file)
+  */
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
